795c08ce147209669121d7537cbc13a4ced0259b
tataaval <avalianitamar2@gmail.com>
2024-04-30 12:38:50 +0400

    Initial Commit

---
+++ test-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-project
+//
+//  Created by Tatarella on 30.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
